3fe739211bdd3b7d1681b41ae6ef6e22660cf88f
CKScripts <78039720+CKScripts@users.noreply.github.com>
2022-04-14 03:53:50 +0000

    Yes

--- index.js
+++ index.js
@@ -2,20 +2,19 @@ const express = require("express");
 const app = express();
 const router = express.Router();
 const path = require("path")
+var port = "4999";
 
 app.use(express.static("Main"));
 
-router.get("/", function(req, res){
-  res.sendFile(path.join(__dirname, "/Main/Directory/Homepage.html"));
+router.get("/", function(req,res){
+  res.sendFile(path.join(__dirname + "/Main/Directory/Homepage.html"));
 });
-app.use("/", router);
 
-app.use(function(req, res, next) {
+app.use("/",router);
+
+app.use(function(req,res,next) {
     res.status(404);
     res.sendFile(__dirname + "/Main/Directory/404.html");
 });
 
-let server = app.listen(3000, function(){
-  console.log("App server is running on port 3000");
-  console.log("to end press Ctrl + C");
-});
+app.listen(port);
